ccbcbcea38ba3f4aa168020c43df9cf8c9f73e6b
Maetha <xnorish@gmail.com>
2019-01-23 17:38:38 +0200

    first styling touches

---
+++ src/components/CharacterPanel.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Character from "./Character";
+
+export default class CharacterPanel extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log(this.state);
+
+    return <div />;
+  }
+}
